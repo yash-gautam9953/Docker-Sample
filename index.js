d6af18f205dbf20a5cfdf665213e871c68b4afd3
Yash Gautam <yashgautam@gmail.com>
2025-08-26 23:46:02 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Docker 🚀");
+  res.send("Node.js on Docker 🚀");
 });
 
 app.listen(PORT, () => {
